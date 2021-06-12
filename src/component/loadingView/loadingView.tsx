f8a7a81d4affe3d72567eb2864c3077208181c76
Yue-plus <Yue_plus@foxmail.com>
2021-06-12 18:45:14 +0800

    add component/loadingView

---
+++ src/component/loadingView/loadingView.tsx
@@ -0,0 +1,12 @@
+import less from './loadingView.less';
+
+export default function LoadingView() {
+  return (
+    <div className={less.loadingView}>
+      <div className={less.loadingCanvasWrapper}>
+        <canvas width="455" height="58" />
+        <div className={less.loadingPercentage}>0 %</div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import less from './index.less';
 import Header from '@/component/_layout/header/header';
+import LoadingView from '@/component/loadingView/loadingView';
+import SlideLogo from '@/component/slideLogo/slideLogo';
 
 export default function IndexPage() {
   return (
-    <div id={less.layout}>
+    <div id={less.layout} className={less.layout} style={{ display: 'block' }}>
       <Header />
+      <LoadingView />
+      <SlideLogo />
+      <div className={`${less.slogan} ${less.show}`}>
+        a world familiarly unknown
+      </div>
     </div>
   );
 }

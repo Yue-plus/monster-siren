edaa8381c5a1005d24c0b8f3ec4dc4bcb117f800
Yue-plus <Yue_plus@foxmail.com>
2021-06-14 19:46:22 +0800

    add components\copyright

---
+++ src/components/_layout/copyright/copyright.tsx
@@ -0,0 +1,5 @@
+import less from './copyright.less';
+
+export default function Copyright() {
+  return <div className={less.copyright}></div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import less from './index.less';
 import Header from '@/components/_layout/header/header';
 import LoadingView from '@/components/loadingView/loadingView';
-import SlideLogo from '@/components/slideLogo/slideLogo';
-import Scroll from '@/components/scroll/scroll';
+import SlideLogo from '@/components/_layout/slideLogo/slideLogo';
+import Scroll from '@/components/_layout/scroll/scroll';
 
 export default function IndexPage() {
   return (
